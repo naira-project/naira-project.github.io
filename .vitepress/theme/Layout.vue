62ba151ea4148dbfa085295ebc05bd1f7d80d04b
mkorbi <16919345+mkorbi@users.noreply.github.com>
2026-03-29 12:00:43 +0000

    Deploying to gh-pages from @ naira-project/naira.github.io@e8b8c194116f6c25a298f87f017bd531172a3958 🚀

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,37 @@
+<template>
+  <Layout>
+    <!-- Add version selector to the navigation bar -->
+    <template #nav-bar-content-after>
+      <VersionSelector />
+    </template>
+
+    <!-- Add YouTube video above homepage feature tiles -->
+    <template #home-features-before>
+    </template>
+  </Layout>
+</template>
+
+<script setup lang="ts">
+import DefaultTheme from 'vitepress/theme'
+import VersionSelector from './components/VersionSelector.vue'
+import mediumZoom from 'medium-zoom'
+import { onMounted, watch, nextTick } from 'vue'
+import { useRoute } from 'vitepress'
+
+const { Layout } = DefaultTheme
+
+const route = useRoute()
+
+const initZoom = () => {
+  mediumZoom('.main img', { background: 'var(--vp-c-bg)', margin: 80 })
+}
+
+onMounted(() => {
+  initZoom()
+})
+
+watch(
+  () => route.path,
+  () => nextTick(() => initZoom())
+)
+</script>
