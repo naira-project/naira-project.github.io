543af7617da7e727f5f2916216c7fe4e25cf29a0
mkorbi <16919345+mkorbi@users.noreply.github.com>
2026-08-06 11:33:57 +0000

    Deploying to gh-pages from @ naira-project/naira-project.github.io@316b8324ec9476283cbfaf10da0fc012fcae2ebc 🚀

---
+++ assets/chunks/swimlanesDiagram-ULZ7WXOC.CdxPBE9i.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN.Cy1yH1xr.js";import{_ as a}from"../app.rEpt0GCe.js";import"./chunk-5VM5RSS4.DJ-cM7K6.js";import"./chunk-XXDRQBXY.CfFe4RYJ.js";import"./chunk-KBJHAD2P.Cim1qGUQ.js";import"./chunk-2GRJ4B5K.tH8e3vif.js";import"./channel.CcUsNs2a.js";import"./framework.B-Nh4q_q.js";import"./theme.BYSCi9k6.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
