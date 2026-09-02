6845aa8ec787f21503ac0225da47f29314bafa20
mkorbi <mkorbi@users.noreply.github.com>
2026-09-02 12:40:35 +0000

    Deploy preview for PR 10 🛫

---
+++ pr-preview/pr-10/assets/chunks/channel.Dym8_XrM.js
@@ -0,0 +1,1 @@
+import{U as a,O as n}from"../app.YWdnp64h.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ pr-preview/pr-10/assets/chunks/funded-EU-IPCEI.DxP42yWD.js
@@ -0,0 +1,1 @@
+const p="/pr-preview/pr-10/blog/funded-EU-IPCEI.png";export{p as _};
