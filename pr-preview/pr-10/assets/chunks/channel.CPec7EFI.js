b51b6c30dcc92b4266d87da9a8d636d15a462d8f
mkorbi <mkorbi@users.noreply.github.com>
2026-09-15 09:00:55 +0000

    Deploy preview for PR 10 🛫

---
+++ pr-preview/pr-10/assets/chunks/channel.CPec7EFI.js
@@ -0,0 +1,1 @@
+import{U as a,O as n}from"../app.C72e07k4.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ pr-preview/pr-10/assets/chunks/swimlanesDiagram-ULZ7WXOC.EUO9phux.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN.CuE7wlIx.js";import{_ as a}from"../app.C72e07k4.js";import"./chunk-5VM5RSS4.CtyIux9F.js";import"./chunk-XXDRQBXY.Disv6T_C.js";import"./chunk-KBJHAD2P.dCEa-cyj.js";import"./chunk-2GRJ4B5K.HGPyzAw7.js";import"./channel.CPec7EFI.js";import"./framework.Dn04Kz81.js";import"./theme.DGADcCi7.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
